c39aa4f67b05c64a11c33fa692926bea64613160
mozso <monoki.zsolt@moriczref.hu>
2025-11-23 13:23:55 +0100

    I. Felvonás kész

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserFactory extends Factory
+{
+    protected $model = User::class;
+
+    public function definition()
+    {
+        $this->faker = \Faker\Factory::create('hu_HU'); // magyar nevekhez
+
+        return [
+            'name' => $this->faker->firstName . ' ' . $this->faker->lastName, // magyaros teljes név
+            'email' => $this->faker->unique()->safeEmail(),
+            'password' => Hash::make('Jelszo123'), // minden user jelszava: Jelszo123
+            'role' => 'student',
+        ];
+    }
+}
